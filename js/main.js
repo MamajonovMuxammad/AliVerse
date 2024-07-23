2b4b4c36ebb05024af71cd50d12faa888d1f6feb
Muxammad <ferzadrotttmagister@gmail.com>
2024-07-23 20:27:33 +0500

    alphatest2

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// function scrollToSection() {
+//   var targetSection = document.getElementById('id_left_block');
+//   targetSection.scrollIntoView({ behavior: 'smooth' });
+// }
+{/* <button onclick="scrollToSection()">Прокрутить к разделу</button> */}
